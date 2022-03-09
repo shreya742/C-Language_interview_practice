f845705907199ead970054bfb8ddcd5950dd845c
Sudireddy Sai Shreya <94278880+shreya742@users.noreply.github.com>
2022-03-09 19:53:13 +0530

    Add files via upload

---
+++ ascii_char.c
@@ -0,0 +1,18 @@
+//PROGRAM TO FIND ASCII VALUE OF CHARACTER.
+
+#include <stdio.h>
+
+void main()
+{
+	char ch;
+	printf("Enter a character : ");
+	scanf(" %c", &ch);
+	printf("ASCII value of %c is %d", ch, ch);
+}
+
+/**
+OUTPUT :
+----------
+Enter a character : S
+ASCII value of S is 83
+**/
